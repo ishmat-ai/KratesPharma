4759c8fd05aa8dc6f2cd94e075ad18aa7c80b43a
ishmat-ai <ishmatfatima321@gmail.com>
2026-05-01 12:41:36 +0530

    Create db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+ini_set('display_errors', 1);  
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "root";  
+$password = "";      
+$dbname = "medapp";  
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);  
+} 
+?>
